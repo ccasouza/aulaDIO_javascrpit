ef1600494cb93e02e25766b2e73fbc5c8ef1328f
Clérison Souza <ccasouza@gmail.com>
2020-05-14 23:42:48 -0300

    Minha primeira página javascript

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+function clicou(){
+    alert("Obrigado por Clicar!")
+}
+
+/*var nome = "Clérison Souza";
+var idade = 39;
+//alert(nome + " tem " + idade + " anos");
+var idade2 = 10;
+var frase = "Brasil é o meu país!"
+alert(nome + " tem " + idade + " anos.");
+console.log(frase);
+console.log(idade + idade2);
+console.log(frase.replace("Brasil", "Pernambuco"));
+*/
+
+/*var lista = ["Maçã", "Pêra", "Laranja"];
+lista.push("Melancia");
+lista.pop();
+console.log(lista);
+console.log(lista.toString());
+alert(lista[0]);
+*/
+
+/*var idade = prompt("Qual a sua idade?");
+if (idade => 18){
+    alert("Maior Idade!")
+}else{
+    alert("Menor idade!")
+}
+*/
+
+/*var count = 0;
+while(count <= 5){
+    console.log(count);
+    alert(count);
+    count++;
+}*/
+
+/*
+var count;
+for(count=0; count<=5; count++){
+    alert(count);
+}*/
+
+/*var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+alert(d.getMonth()+1);*/
